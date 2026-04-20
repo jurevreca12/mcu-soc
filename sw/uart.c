4c6e1ba685ebddc04098705b6ac3cd153f60c6d7
jvreca <jure.vreca@ijs.si>
2026-04-20 11:30:27 +0000

    defs for uart.c

--- sw/uart.c
+++ sw/uart.c
@@ -1,6 +1,14 @@
-
+#define UART_CONF_OFF    0x0
+#define UART_SPEED_OFF   0x4
+#define UART_TX_OFF      0x8
+#define UART_RX_OFF      0xC
+#define UART_BASE_ADDR   0x40000000
+#define UART_CONF_ADDR   (UART_BASE_ADDR + UART_CONF_OFF)
+#define UART_SPEED_ADDR  (UART_BASE_ADDR + UART_SPEED_OFF)
+#define UART_TX_ADDR     (UART_BASE_ADDR + UART_TX_OFF)
+#define UART_RX_ADDR     (UART_BASE_ADDR + UART_RX_OFF)
 
 int main() {
-
+    
     return 0;
 }
